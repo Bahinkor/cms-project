96df37b09c2ec2e2597f1ef3bd3b04d70fbaa6b1
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-06-22 17:24:01 +0330

    new page (product page)

---
+++ src/pages/product/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Product() {
+    // JSX
+    return (
+        <div>
+            ppp
+        </div>
+    );
+}
+
+export default Product;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Home from './pages/home/Home';
 import UserList from './pages/users/UserList';
 import NewUser from './pages/newUser/NewUser';
 import Products from './pages/products/Products';
+import Product from './pages/product/Product';
 import NotFound from './pages/notFound/NotFound';
 
 let routes = [
@@ -9,6 +10,7 @@ let routes = [
     {path: "/users", element: <UserList/>},
     {path: "/newuser", element: <NewUser/>},
     {path: "/products", element: <Products/>},
+    {path: "/product/:productID", element: <Product/>},
     {path: "*", element: <NotFound/>},
 ];
 
