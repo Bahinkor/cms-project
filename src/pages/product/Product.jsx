60e5204c705d39ae7c67f48b6c0b45f277061972
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-06-22 17:47:58 +0330

    Development header product page

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Product() {
     // JSX
     return (
-        <div>
-            ppp
+        <div className="flex-[4] p-5">
+            <div className="flex items-center justify-between">
+                <h1 className="text-3xl font-bold">Product</h1>
+                <Link to="/newProduct">
+                    <button
+                        className="w-20 p-[5px] bg-blue-700 text-white text-lg rounded-[5px] cursor-pointer">Create
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 }
