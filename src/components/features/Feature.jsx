908b2e4f77c81195b863adc2de55e698440c0768
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-06-23 18:34:34 +0330

    dynamiced features component

--- src/components/features/Feature.jsx
+++ src/components/features/Feature.jsx
@@ -1,56 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ArrowDownward} from "@mui/icons-material";
 import {ArrowUpward} from "@mui/icons-material";
+import featuresData from "../../data-dir/featuresData";
 
 function Feature() {
+    // Sates
+    const [festursDataState, setFestursDataState] = useState(featuresData);
+
     // JSX
     return (
         <div
             className="flex justify-between w-[77rem] [&>div]:flex-1 [&>div]:mx-5 [&>div]:p-[30px] [&>div]:rounded-[10px] [&>div]:cursor-pointer [&>div]:box-shadow-feature-item">
-            {/* Revalue item */}
-            <div>
-                <h4 className="text-[20px] text-gray-600">Revalue</h4>
-                <div className="flex items-center my-[10px]">
-                    <span className="text-[30px] font-[600]">$2,412</span>
-                    <span className="flex items-center ml-5">
-                        -11.4
-                        <ArrowDownward className="text-[12px] text-red-700 ml-[5px]"/>
-                    </span>
-                </div>
-                <span className="text-[15px] text-gray-400">
-                    Compared to last month
-                </span>
-            </div>
-
-            {/* Sales item */}
-            <div>
-                <h4 className="text-[20px] text-gray-600">Sales</h4>
-                <div className="flex items-center my-[10px]">
-                    <span className="text-[30px] font-[600]">$4,415</span>
-                    <span className="flex items-center ml-5">
-                        -1.4
-                        <ArrowDownward className="text-[12px] text-red-700 ml-[5px]"/>
-                    </span>
-                </div>
-                <span className="text-[15px] text-gray-400">
-                    Compared to last month
-                </span>
-            </div>
-
-            {/* Cost item */}
-            <div>
-                <h4 className="text-[20px] text-gray-600">Cost</h4>
-                <div className="flex items-center my-[10px]">
-                    <span className="text-[30px] font-[600]">$2,225</span>
-                    <span className="flex items-center ml-5">
-                        +2.4
-                        <ArrowUpward className="text-[12px] text-green-700 ml-[5px]"/>
-                    </span>
-                </div>
-                <span className="text-[15px] text-gray-400">
-                    Compared to last month
-                </span>
-            </div>
+            {/* item */}
+            {
+                festursDataState.map((item) => (
+                    <div>
+                        <h4 className="text-[20px] text-gray-600">{item.title}</h4>
+                        <div className="flex items-center my-[10px]">
+                            <span className="text-[30px] font-[600]">${item.num.toLocaleString()}</span>
+                            <span className="flex items-center ml-5">
+                                {item.growth}
+                                {
+                                    item.growth > 0 ? (
+                                        <ArrowUpward className="text-[12px] text-green-700 ml-[5px]"/>
+                                    ) : (
+                                        <ArrowDownward className="text-[12px] text-red-700 ml-[5px]"/>
+                                    )
+                                }
+                            </span>
+                        </div>
+                        <span className="text-[15px] text-gray-400">
+                            {item.desc}
+                        </span>
+                    </div>
+                ))
+            }
         </div>
     );
 }
---
+++ src/data-dir/featuresData.js
@@ -0,0 +1,22 @@
+const featuresData = [
+    {
+        title: "Revalue",
+        num: 2_412,
+        desc: "Compared to last month",
+        growth: -11.4
+    },
+    {
+        title: "Sales",
+        num: 4_415,
+        desc: "Compared to last month",
+        growth: -1.4
+    },
+    {
+        title: "Cost",
+        num: 2_225,
+        desc: "Compared to last month",
+        growth: +2.4
+    },
+];
+
+export default featuresData;
